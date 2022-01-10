bba23bf41f1ea39abcb9f491d64f91a58b1a21a6
José Juan Ojeda Granados <jotafirebase@gmail.com>
2022-01-10 22:03:14 +0000

    Actualizaciones

--- Repa02.cpp
+++ Repa02.cpp
@@ -13,43 +13,47 @@
 */
 #include <iostream>
 
-int funsionPunteros(int *punA, int *punB)
+void funsionPunteros(int *punA, int *punB)
 {
+    printf("\e[0;33mFunción Punteros\e[0m\n");
+    printf("\e[0;34mSumamos 10 a cada variable\e[0m\n");
+    printf("*punA\t%p\n", punA);
+    *punA = *punA + 10;
+    printf("*punB\t%p\n", punB);
+    *punB = *punB + 10;
+    printf("punA\t%d\n", *punA);
+    printf("punB\t%d\n", *punB);
 
-    return 0;
+    printf("\e[0;34mpunA y punB son variables locales de la función\e[0m\n");
+
+    // return;
 }
 
 int main(int argc, char *argv[])
 {
     system("clear");
+    printf("\e[0;33mFunción Main\e[0m\n");
 
     printf("\e[0;34mSe crean varA y varB y se almacena los valores 20 y 40 respectivamente\e[0m\n");
     int varA = 20;
     int varB = 40;
+    printf("varA =\t%d\n", varA);
+    printf("varB =\t%d\n", varB);
 
     int *punA = nullptr;
-    printf("*punA\t%p\n", punA);
+    int *punB = nullptr;
+    printf("*punA = nullptr\t%p\n", punA);
+    printf("*punB = nullptr\t%p\n", punB);
 
+    printf("\e[0;34mSe llama a la función Punteros pasando las direcciones de memoria de varA y varB\e[0m\n");
+    funsionPunteros(&varA, &varB);
+
+    printf("\e[0;33mContinua Función Main\e[0m\n");
+    printf("\e[0;34mSe muestra el nuevo valor de varA y varB modificados por la función\e[0m\n");
     printf("varA\t%d\n", varA);
-    printf("\e[0;34mSe almacena en punA la dirección de memoria que el sistema asigno a varA\e[0m\n");
-    punA = &varA;
-    printf("\e[0;34mMostramos la dirección de memoria de varA y su contenido\e[0m\n");
-    printf("punA\t%p\n", punA);
-    printf("*punA\t%d\n", *punA);
-
-    printf("\n\t\e[0;33mMemoria Heap\e[0m\n");
-    printf("\e[0;34mSe crea *punB y se devuelve la dirección de memoria heap asignada en él\e[0m\n");
-    int *punB = new int;
-    printf("*punB\t%p\n", punB);
-    printf("\e[0;34mPor desreferencia se almacena en esa dirección de memoria el valor 40\e[0m\n");
-    *punB = 40;
-    printf("\e[0;34mMostramos nuevamente la dirección de memoria y su contenido\e[0m\n");
-    printf("punB\t%p\n", punB);
-    printf("*punB\t%d\n", *punB);
-
-    printf("\e[0;34mSe borra el contenido del puntero y el puntero con delete\e[0m\n\n");
-    punB = nullptr;
-    delete punB;
+    printf("varB\t%d\n", varB);
+    printf("*punA = nullptr\t%p\n", punA);
+    printf("*punB = nullptr\t%p\n", punB);
 
     return 0;
 }
