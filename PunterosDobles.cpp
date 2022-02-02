080be9f50ca7ce42940a37fdc10cdd7afd472421
José Juan Ojeda Granados <jotafirebase@gmail.com>
2022-02-02 22:54:36 +0000

    Actualizaciones

--- PunterosDobles.cpp
+++ PunterosDobles.cpp
@@ -16,13 +16,12 @@
 void funsionPuntero(int *punA)
 {
     printf("\n\e[0;33mFunción Puntero\e[0m\n");
-    printf("\e[0;34mSumamos 10 a la variable\e[0m\n");
-    printf("punA:\t%p\n", punA);
+    printf("\e[0;36mpunA es variable local de la función\e[0m\n");
+    // Inicializada con la dirección pasada al llamar a la función
+    printf("Contenido de punA Local:\t%p\n", punA);
+    printf("\e[0;36mSumamos 10 a la variable (*punA)\e[0m\n");
     *punA = *punA + 10;
-    printf("*punA = *punA + 10\t%d\n", *punA);
-
-    printf("\e[0;34mpunA es variable local de la función\e[0m\n");
-    printf("\e[0;34mInicializa con la dirección pasada al llamar a la función\e[0m\n");
+    printf("*punA = *punA + 10 =\t%d\n", *punA);
 
     // return; // no necesario al no devolver ningún valor ;-)
 }
@@ -30,12 +29,14 @@ void funsionPuntero(int *punA)
 void funsionPunteroDoble(int **punB)
 {
     printf("\n\e[0;33mFunción Puntero Doble\e[0m\n");
-    printf("\e[0;34mSumamos 10 a la variable\e[0m\n");
-    printf("punB:\t%p\n", *punB);
+    printf("\e[0;36mpunB es variable local de la función\e[0m\n");
+    // Inicializada con la dirección pasada al llamar a la función
+    printf("Dirección de **punB local\t%p\n", &punB);
+    printf("Contenido de **punB local\t%p\n", *punB);
+    printf("\e[0;36mSumamos 10 a la variable (**punB)\e[0m\n");
     **punB = **punB + 10;
-    printf("**punB = **punB + 10\t%d\n", **punB);
+    printf("**punB = **punB + 10 =\t%d\n", **punB);
 
-    printf("\e[0;34mpunB es variable local de la función\e[0m\n");
 
     // return; // no necesario al no devolver ningún valor ;-)
 }
@@ -43,12 +44,13 @@ void funsionPunteroDoble(int **punB)
 void funsionPunteroMemDinamica(int **punC)
 {
     printf("\n\e[0;33mFunción Puntero Doble Memoria Dinámica\e[0m\n");
-    printf("\e[0;34mSumamos 10 a la variable\e[0m\n");
-    printf("punC:\t%p\n", *punC);
+    printf("\e[0;36mpunC es variable local de la función\e[0m\n");
+    printf("\e[0;36mSumamos 10 a la variable\e[0m\n");
+    printf("Dirección de **punC local\t%p\n", &punC);
+    printf("Contenido de **punC local\t%p\n", *punC);
     **punC = **punC + 10;
-    printf("**punC = **punC + 10\t%d\n", **punC);
+    printf("**punC = **punC + 10 =\t%d\n", **punC);
 
-    printf("\e[0;34mpunC es variable local de la función\e[0m\n");
 
     // return; // no necesario al no devolver ningún valor ;-)
 }
@@ -58,7 +60,7 @@ int main(int argc, char *argv[])
     system("clear");
     printf("\e[0;33mFunción Main\e[0m\n");
 
-    printf("\e[0;34mSe crean varA y varB y se almacena los valores 20 y 40 respectivamente\e[0m\n");
+    printf("\e[0;36mSe crean varA y varB y se almacena los valores 20 y 40 respectivamente\e[0m\n");
     int varA = 20;
     int varB = 40;
     printf("int varA =\t%d\n", varA);
@@ -67,38 +69,37 @@ int main(int argc, char *argv[])
     int *punA = nullptr;
     int *punB{&varB};;
     printf("*punA = nullptr\t%p\n", punA);
-    printf("*punB = Dirección memoria de varB\t%p\n", punB);
+    printf("*punB = Dirección de varB\t%p\n", punB);
 
-    printf("\e[0;34mLlamada a la función Puntero pasando la dirección de memoria de varA\e[0m\n");
+    printf("\e[0;36mLlamada a la función Puntero pasando la dirección de memoria de varA\e[0m\n");
     funsionPuntero(&varA);
 
-    printf("\e[0;34mLlamada a la función Puntero Doble pasando la dirección de memoria\n");
-    printf("de punB que a su vez contiene la dirección de varA\e[0m\n");
+    printf("\n\e[0;33mContinua Función Main\e[0m\n");
+    printf("\e[0;36mLlamada a la función Puntero Doble pasando la dirección de memoria\n");
+    printf("de punB que a su vez contiene la dirección de varB\e[0m\n");
     funsionPunteroDoble(&punB);
 
     printf("\n\e[0;33mContinua Función Main\e[0m\n");
-    printf("\e[0;34mNuevos valores de varA y varB modificados por las funciones\e[0m\n");
+    printf("\e[0;36mNuevos valores de varA y varB modificados por las funciones\e[0m\n");
     printf("varA =\t%d\n", varA);
     printf("varB =\t%d\n", varB);
 
-    printf("\n\e[0;36mMemoria Heap\e[0m\n");
-    printf("\e[0;34mSe crea *punC y se devuelve la dirección de memoria heap asignada en él\e[0m\n");
+    printf("\n\e[0;35mMemoria Heap\e[0m\n");
+    printf("\e[0;36mSe crea *punC y se devuelve la dirección de memoria heap asignada en él\e[0m\n");
     int *punC = new int;
-    printf("*punC\t%p\n", punC);
-    printf("\e[0;34mPor desreferencia se almacena en esa dirección de memoria el valor 60\e[0m\n");
+    printf("Dirección de *punC\t\t%p\n", &punC);
+    printf("Dirección guardada en *punC\t%p\n", punC);
+    printf("\e[0;36mPor desreferencia se almacena en esa dirección de memoria el valor 60\e[0m\n");
     *punC = 60;
-    printf("\e[0;34mMostramos nuevamente la dirección de memoria y su contenido\e[0m\n");
-    printf("punC\t%p\n", punC);
-    printf("*punC\t%d\n", *punC);
+    printf("\e[0;36mMostramos nuevamente la dirección de memoria y su contenido\e[0m\n");
+    printf("Contenido en la dirección\t%p = %d\n",punC, *punC);
 
     funsionPunteroMemDinamica(&punC);
     
     printf("\n\e[0;33mContinua Función Main\e[0m\n");
-    printf("\e[0;34mMostramos nuevamente la dirección de memoria y su contenido\e[0m\n");
-    printf("punC\t%p\n", punC);
-    printf("*punC\t%d\n", *punC);
-
-    printf("\e[0;34mSe borra el contenido del puntero y el puntero con delete\e[0m\n\n");
+    printf("\e[0;36mMostramos nuevamente la dirección de memoria y su contenido\e[0m\n");
+    printf("Contenido en la dirección\t%p = %d\n",punC, *punC);
+    printf("\e[0;36mSe borra el contenido del puntero y el puntero con delete\e[0m\n\n");
     punC = nullptr;
     delete punC;
 
